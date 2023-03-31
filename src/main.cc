e36918d146a6d938a85a07f2899b8d94f8dec7ae
fedorovee <inferngeek@gmail.com>
2023-03-31 03:49:18 +0300

    Initial commit

---
+++ src/main.cc
@@ -0,0 +1,12 @@
+/* Copyright (c) 2023 Emil Fedorov
+ *
+ * Webtoon Concatenator or simply WTCat.
+ * 
+ * WTCat is an open-source and, I hope, cross-platfrom application
+ * for concatenating sliced images of Webtoon pages.
+ * 
+ * Perhaps in the future I'll develop full toolkit for downloading
+ * and preparing webtoons for scanlating.
+ * 
+ */
+
