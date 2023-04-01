57f9a33c389f53913e4dcb172fcadeec831385ac
fedorovee <inferngeek@gmail.com>
2023-04-01 14:07:45 +0300

    README.MD and TODO.MD have been changed to README and TODO, respectively. The build folder is no longer ignored.

--- src/main.cc
+++ src/main.cc
@@ -3,10 +3,16 @@
  * Webtoon Concatenator or simply WTCat.
  * 
  * WTCat is an open-source and, I hope, cross-platfrom application
- * for concatenating sliced images of Webtoon pages.
+ * 
  * 
  * Perhaps in the future I'll develop full toolkit for downloading
  * and preparing webtoons for scanlating.
  * 
  */
 
+#include <iostream>
+#include "cli.h"
+
+int main(int argc, const char** argv, const char** envp) {
+    cli::parseArgs(argc, argv, envp);
+}
