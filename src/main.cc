f458aa305b5904543755d056d8b30a8e28946a39
fedorovee <inferngeek@gmail.com>
2023-04-01 14:09:46 +0300

    Deleted line in main.cc have been returned. Adding header in README

--- src/main.cc
+++ src/main.cc
@@ -3,7 +3,7 @@
  * Webtoon Concatenator or simply WTCat.
  * 
  * WTCat is an open-source and, I hope, cross-platfrom application
- * 
+ * for concatenating sliced images of Webtoon pages.
  * 
  * Perhaps in the future I'll develop full toolkit for downloading
  * and preparing webtoons for scanlating.
