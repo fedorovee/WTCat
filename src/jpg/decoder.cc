03e11fb6695c15eb9e623d725118a773faab23e7
Fedorov E.E <inferngeek@gmail.com>
2023-04-02 16:16:02 +0300

    Decoder edited

--- src/jpg/decoder.cc
+++ src/jpg/decoder.cc
@@ -6,58 +6,73 @@
 namespace jpegUtils
 {
 
-void JPEG::ReadFromFile() {
-    unsigned char byte = 0x00;
+namespace Markers
+{
+    const unsigned char JPEG_FORMAT[3] = {0xFF, 0xD8, 0x00};
+} // namespace Markers
+
+bool hexstrcmp(const char* left, const char* right) {
+    for (int i = 0; ; i++) {
+        if (left[i] != right[i]) 
+            return 0;
+        else if (left[i] == '\0' && right[i] == '\0')
+            return 1;
+    }
+
+    return 1;
+}
+
+int JPEG::ReadFromFile() {
     int segment_number = 0;
     int len_of_segment = 2;
     int len_of_new_seg = 0;
-    bool jpeg_signature = false;
+    bool len_reading = false;
+
+    vector <string> seg;
     // bool begin_of_segment = true;
     
     int i = 0;
     while (!file->eof()) {
-        byte = file->get();
+        std::cout << "Deb#3\n";
+        seg[segment_number] += file->get();
 
-        std::cout << std::hex << std::uppercase << (int) byte << ' ';
+        if (i >= 2) {
+            
+            if (len_of_segment == 0) {
+                if (!len_of_new_seg) {
+                    segment_number++;
+                    len_of_segment = 4;
+                    len_reading = true;
+                }
+                else {
+                    len_of_segment = len_of_new_seg;
+                    len_reading = false;
+                }
+                len_of_new_seg = 0;
 
-        if (i == 0 && byte == 0xFF) {
-            jpeg_signature = true;
-        } else if(!jpeg_signature || i == 1 && byte != 0xD8) {
-            jpeg_signature = false;
-            init_error = 1;
-            break;
-        } //else {
-        //     if (len_of_segment == 0) {
-        //         if (!len_of_new_seg) {
-        //             len_of_segment = 4;
-        //             segment_number++;
-        //         }
-        //         else
-        //             len_of_segment = len_of_new_seg;
-        //         len_of_new_seg = 0;
-        //     } else if (len_of_segment <= 2) {
-        //         len_of_new_seg += byte;
-        //     }
-        // }
+            } else if (len_of_segment <= 2 && len_reading) {
+                len_of_new_seg += seg[segment_number][i];
+            }
 
-        // if (segments.size() < (segment_number - 1))
-        //     segments.push_back("" + byte);
-        // else
-        //     segments[segment_number] += byte;
+        } else if (i == 1 && !(seg[0][i - 1] == 0xFF && seg[0][i] == 0xD8)) {
+            return 2;
+        }
 
-        // len_of_segment--;
-        // i++;
+        i++;
+        len_of_segment--;
     }
+
+    return 0;
 }
 
 JPEG::JPEG(const char* path) :
     file(new std::ifstream(path, std::ios::binary | std::ios::in)) {
     if (file->is_open()) {
         std::cout << "Deb#1\n";
-        ReadFromFile();
+        init_error = ReadFromFile();
     } else {
         std::cout << "Deb#2\n";
-        init_error = 0;
+        init_error = 1;
     }
 }
 
